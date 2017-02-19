a02b61de7567fd13f26fced73c1039ae2090e8e3
Spean92 <egor.muhladi@gmail.com>
2017-02-20 01:19:24 +0200

    home_page desctop, clients_page desctop, other pages - html only

---
+++ app/js/common.js
@@ -0,0 +1,125 @@
+$(document).ready(function() {
+
+// Цвет полоски меню
+	var box1 = $('.box-1').height();
+	var box2 = $('.box-2>div').height()
+	var box3 = $('.box-3').height();
+	var pos1 = box1;
+	var pos2 = pos1 + box2;
+	var pos3 = pos2 + box3;
+	var finalPos1 = pos1 + -'77';
+	var finalPos2 = pos2 + -'15';
+	var finalPos3 = pos3;
+
+	window.onscroll = function(){
+
+	   var html = document.documentElement, body = document.body;
+		 if (body.scrollTop<finalPos1){
+			 $('#homeMenu').css('display','inline-block');
+			 $('#aboutMenu').css('display','none');
+			 $('#clientsMenu').css('display','none');
+			 $('header').removeClass('active');
+			 $('header').css('background', 'rgba(0, 0, 0, 0.3)');
+			 $('header #logo').attr('src', 'img/evestudio_logo_r_whire.svg');
+		 } else if (body.scrollTop>finalPos1 && body.scrollTop<pos1) {
+			 $('header').addClass('active');
+			 $('header #logo').attr('src', 'img/evestudio_logo_r.svg');
+			 $('header.active').css('background', 'rgba(255, 255, 255, 1)');
+		 }else if(body.scrollTop>finalPos1 && body.scrollTop<finalPos2) {
+			 $('#homeMenu').css('display','none');
+			 $('#aboutMenu').css('display','inline-block');
+			 $('#clientsMenu').css('display','none');
+			 $('header').addClass('active');
+			 $('header.active').css('background', 'rgba(255, 255, 255, 0.7)');
+			 $('header #logo').attr('src', 'img/evestudio_logo_r.svg');
+	   } else if (body.scrollTop>finalPos2) {
+			 $('#homeMenu').css('display','none');
+			 $('#aboutMenu').css('display','none');
+			 $('#clientsMenu').css('display','inline-block');
+			 $('header').addClass('active');
+			 $('header #logo').attr('src', 'img/evestudio_logo_r.svg');
+
+		 }
+		//  console.log(body.scrollTop);
+  };
+
+// Меню
+	var menu = 0;
+	$('header .right div.bars').click(function(){
+		if (menu == 0) {
+			// $('#box-2, #box-3, footer').fadeOut();
+			$('body').css("overflow", "hidden");
+			$('main nav.nav').fadeIn(300);
+			$('header .right div.bars').css('border', 'none');
+			$('header .right div.bars::before').css('border', 'none');
+			$('header .right div.bars').css('background', 'url(img/close.svg) no-repeat center');
+			$('header .right div.bars').removeClass('bars1');
+			$('#aboutMenu').css('display','none');
+			$('#clientsMenu').css('display','none');
+			$('#homeMenu').fadeIn(200);
+			$('header').removeClass('active');
+			$('header').css('background', 'rgba(0, 0, 0, 0.4)');
+			$('header #logo').attr('src', 'img/evestudio_logo_r_whire.svg');
+			menu = 1;
+		} else {
+			$('main nav.nav').fadeOut(300);
+			$('body').css("overflow", "auto");
+			// $('#box-2, #box-3, footer').fadeIn();
+			$('header .right div.bars').css({'border-top': '2px solid white', 'border-bottom': '2px solid white'});
+			$('header .right div.bars').css('background', 'none');
+			$('header .right div.bars').addClass('bars1');
+
+			menu = 0;
+		}
+	});
+	$('.nav .container a').click(function(){
+		$('main nav.nav').fadeOut(300);
+		$('body').css("overflow", "auto");
+		$('header').addClass('active');
+		$('header.active').css('background', 'rgba(255, 255, 255, 0.7)');
+		// $('#box-2, #box-3').fadeIn();
+		// $('#box-2, #box-3, footer').fadeIn();
+		$('header .right div.bars').css({'border-top': '2px solid white', 'border-bottom': '2px solid white'});
+		$('header .right div.bars').css('background', 'none');
+		$('header .right div.bars').addClass('bars1');
+
+		menu = 0;
+
+	});
+
+	//Кнопка "Наверх"
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 1
+		}, 800);
+		return false;
+	});
+
+// OWL
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		// jsonPath : 'customData.json'
+		navigation : true,
+		slideSpeed : 300,
+		mouseDrag : true,
+		startPosition : 2,
+		paginationSpeed : 400,
+		      singleItem:true,
+					loop:true,
+		      items : 1,
+	});
+
+// black/color IMG
+	$('.lid a img').hover(function() {
+	 var source =	$(this).attr('src');
+		$(this).attr('src', source.slice(0, -10)+'.svg');
+		var hi = $(this).attr('src');
+	},
+	function() {
+		var source =	$(this).attr('src');
+		$(this).attr('src', source.slice(0, -4)+'_black.svg');
+		var hu = $(this).attr('src');
+	});
+
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var gulp = require('gulp'),
+    concatCss = require('gulp-concat-css'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    browserSync  = require('browser-sync'),
+    rename = require('gulp-rename'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer');
+
+//
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'dest'
+        },
+        notify: false
+    });
+});
+// html
+gulp.task('html', function() {
+  return gulp.src('app/*.html')
+  .pipe(gulp.dest('dest'))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+// scss
+gulp.task('scss', function() {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 20 versions'))
+    .pipe(cleanCSS())
+    // .pipe(rename("style.min.css"))
+    .pipe(gulp.dest('dest/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// scripts
+gulp.task('scripts', function() {
+  return gulp.src('app/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dest/js'))
+    .pipe(browserSync.reload({stream: true}));
+
+});
+
+// images
+gulp.task('images', function() {
+  return gulp.src('app/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dest/img'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// fonts
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dest/fonts'));
+});
+
+// watcher
+gulp.task('watch',['browser-sync'], function () {
+    gulp.watch('app/scss/**/*', ['scss'])
+    gulp.watch('app/js/*', ['scripts'])
+    gulp.watch('app/*.html', ['html']);
+
+});
+
+// default
+gulp.task('default', ['images', 'watch', 'fonts']);
